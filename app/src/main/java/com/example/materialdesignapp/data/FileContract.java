babe83fb0e22f11f939105659760707010b531e2
Ярослав <powerman23rus@gmail.com>
2018-01-29 14:00:04 +0300

    add nit uploaded files

---
+++ app/src/main/java/com/example/materialdesignapp/data/FileContract.java
@@ -0,0 +1,16 @@
+package com.example.materialdesignapp.data;
+
+import android.net.Uri;
+
+/**
+ * Created by powerman23rus on 29.01.2018.
+ * Enjoy ;)
+ */
+
+public class FileContract {
+    public static String AUTHORITY = AppContentProvider.AUTHORITY + ".fileprovider";
+
+    public static class CacheFiles {
+        public static String PATH_TALES = "tales";
+    }
+}
